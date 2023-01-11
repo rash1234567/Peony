4339a97baac3fadf0e1fdcd76c11d268f5f4ef51
Okunade sherif <o.d.alsherif@gmail.com>
2023-01-11 17:35:10 +0100

    final commit

--- src/Components/AboutUs/AboutUs.jsx
+++ src/Components/AboutUs/AboutUs.jsx
@@ -4,7 +4,7 @@ import indexStyles from '../../styles/index.module.css'
 
 function AboutUs() {
   return (
-    <div className={`flex flex-col mt-[20%] mb-[10%] lg:mb-[5%] xl:mt-[20%] xl:mb-[10vh] justify-between h-[650px] md:h-[700px] bg-green-700 lg:h-[600px] xl:h-[100vh] lg:flex-row xl:pr-8 ${indexStyles.AboutUsDiv}`}>
+    <div className={`flex flex-col mt-[20%] mb-[10%] lg:mb-[5%] xl:mt-[20%] xl:mb-[10vh] justify-between h-[650px] md:h-[700px] lg:h-[600px] xl:h-[100vh] lg:flex-row xl:pr-8 ${indexStyles.AboutUsDiv}`}>
         <div className='relative  md:right-[15vh] lg:w-[40%] xl:w-[50%] h-[45%] xl:h-[90%] md:h-[40%] xl:mt-[5%] lg:mt-[10%]'>
             <div className='bg-[#C4F4D7] right-[2vh] lg:-right-[13vh] xl:-right-[15vh] xl:-top-[3vh] absolute h-[256px] w-[240px] xl:w-[40%] xl:h-[40%] 2xl:w-[40%] 2xl:h-[40%] rounded-tr-[10px] rounded-br-[10px]'></div>
             <img src={img} className='absolute bg-white w-4/5 top-[4vh] lg:top-[3vh] xl:top-[4vh] lg:left-[15vh] xl:left-[20vh] right-[5vh] md:w-[400px] lg:w-[350px] xl:w-[85%] x'/>
--- src/Components/Heroe/Heroe.jsx
+++ src/Components/Heroe/Heroe.jsx
@@ -10,7 +10,7 @@ function Heroe() {
               <p className={`font-openSans font-[600] text-[12px] md:text-[20px] 3xl:text-[28px] 4xl:text-[35px] 3xl:leading-[40px] 4xl:leading-[50px] md:leading-[27px] text-white ${heroStyles.heroText}`}>Automate all work and approval flow <br /> with Electronic Document Management <br />System </p>
               <div className={`w-[80%] lg:w-[60%]  flex justify-between ${heroStyles.btns}`}>
                 <button className={`bg-[#04E761] hover:bg-[#1FBE54] text-white w-[100px] md:w-[180px] h-[30px] 3xl:w-[45%] 3xl:h-[70px] md:h-[50px] rounded-md text-[12px] md:text-[16px] 3xl:text-[28px] 4xl:text-[32px] font-[700] font-openSans ${heroStyles.gsBtn}`}>Get Started</button>
-                <button className={`text-white text-[12px] md:text-[16px] 3xl:text-[28px] 4xl:text-[32px] font-[700] font-openSans ${heroStyles.wvBtn} hover:bg-[#004A00] w-[100px] md:w-[180px] `}><i class="fa-solid fa-play mr-1 md:mr-3 "></i> Watch Video</button>
+                <button className={`text-white text-[12px] md:text-[16px] 3xl:text-[28px] 4xl:text-[32px] font-[700] font-openSans ${heroStyles.wvBtn} hover:bg-[#004A00] w-[45%] `}><i class="fa-solid fa-play mr-1 md:mr-3 "></i> Watch Video</button>
               </div>
             </div>
           </div>

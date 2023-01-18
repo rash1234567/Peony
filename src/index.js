bd0c22ee5b702857a6b87191c68d9c711c30dd62
Okunade sherif <o.d.alsherif@gmail.com>
2023-01-18 05:37:32 +0100

    context

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ import { CartContextProvider } from './Utils/CartContextProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CartContextProvider>
        <App />
-    </CartContextProvider>
   </React.StrictMode>
 );
